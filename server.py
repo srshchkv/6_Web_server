5a4ae26781d6925485b9466233068131c2b63fc9
srshchkv <125016193+srshchkv@users.noreply.github.com>
2024-06-09 22:58:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,47 @@
 import socket
+import threading
+import os
 
-sock = socket.socket()
+def handle_client(conn):
+    try:
+        data = conn.recv(8192)
+        request = data.decode()
+        headers = request.split('\n')
+        filename = headers[0].split()[1]
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+        if filename == '/':
+            filename = '/index.html'
 
-sock.listen(5)
+        filepath = '.' + filename
+        if os.path.exists(filepath) and filename.endswith(('.html', '.css', '.js')):
+            with open(filepath, 'rb') as file:
+                response = file.read()
+            status_line = "HTTP/1.1 200 OK\n"
+            content_type = "Content-Type: text/html\n"
+        else:
+            response = b"<h1>404 Not Found</h1>"
+            status_line = "HTTP/1.1 404 Not Found\n"
+            content_type = "Content-Type: text/html\n"
 
-conn, addr = sock.accept()
-print("Connected", addr)
+        conn.sendall(status_line.encode())
+        conn.sendall(content_type.encode())
+        conn.sendall(b"\n")
+        conn.sendall(response)
+    finally:
+        conn.close()
 
-data = conn.recv(8192)
-msg = data.decode()
+def start_server(port=8080):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', port))
+    sock.listen(5)
+    print(f"Server started on port {port}...")
 
-print(msg)
+    while True:
+        conn, addr = sock.accept()
+        print(f"Connected by {addr}")
+        client_thread = threading.Thread(target=handle_client, args=(conn,))
+        client_thread.start()
 
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+if __name__ == "__main__":
+    start_server()
